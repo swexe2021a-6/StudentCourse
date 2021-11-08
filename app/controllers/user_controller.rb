286f45a50f31920340cb25e164097757a0d65bc6
RikiOgita <1911140059k@ed.fuk.kindai.ac.jp>
2021-11-08 07:12:22 +0000

    userController create

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,29 @@
 class UserController < ApplicationController
+    def index
+        @user = User.all
+    end
+    def new
+        @user = User.new
+    end
+    def create
+        logger.debug "---------------"
+        @user = User.new(student_num: params[:user][:student_num],
+        password: params[:user][:password],
+        password_confirmation: params[:user][:password_confirmation])
+        if @user.valid?
+          flash[:notice] = 'ユーザ登録完了'
+          @user.pass = BCrypt::Password.create(params[:user][:password])
+          @user.save
+          redirect_to user_path
+        else
+          render 'new'
+        end
+    end
+    def destroy
+        user = User.find(params[:id])
+        if user.destroy
+          flash[:notice] = 'ユーザを削除しました'
+        end
+        redirect_to users_path
+    end
 end
