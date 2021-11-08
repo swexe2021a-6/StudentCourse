725eab63b400eb58d4a277231241c64334ae5bae
RikiOgita <1911140059k@ed.fuk.kindai.ac.jp>
2021-11-08 08:07:17 +0000

    valuationTestCommit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-#ActiveRecord::Schema.define(version: 0) do
 ActiveRecord::Schema.define(version: 2021_11_08_062645) do
 
   create_table "users", force: :cascade do |t|
@@ -26,5 +25,5 @@ ActiveRecord::Schema.define(version: 2021_11_08_062645) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
-#end
