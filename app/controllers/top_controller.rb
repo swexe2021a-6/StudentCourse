8a254b17acc7fcc6483e2b07226634e34dd02f5b
RikiOgita <1911140059k@ed.fuk.kindai.ac.jp>
2021-11-08 07:22:36 +0000

    topControllerFix

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,24 @@
 class TopController < ApplicationController
     def main
-        
+        render 'login'
     end
+
+  def login
+    user = User.find_by(uid: params[:uid])
+    if user and BCrypt::Password.new(user.pass) == params[:pass]
+      flash[:notice] = 'ログイン成功'
+      session[:login_uid] = user.uid
+      redirect_to root_path
+    else
+      flash[:notice] = 'ログイン失敗'
+      render 'login'
+    end
+  end
+
+  def logout
+    flash[:notice] = 'ログアウトに成功'
+    session.delete(:login_uid)
+    
+    redirect_to root_path
+  end
 end
