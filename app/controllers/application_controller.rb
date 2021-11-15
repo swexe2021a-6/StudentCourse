380e2b3b9133e623ab3347eb0e2e09ad76503315
kambara <1911140015d@ed.fuk.kindai.ac.jp>
2021-11-15 07:50:50 +0000

    current

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    private
+        def current_user
+            if session[:login_student_num]
+                User.find_by(uid: session[:login_student_num])
+            end
+        end
+    helper_method :current_user
 end
