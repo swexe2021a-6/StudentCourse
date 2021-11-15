2a1289d477d6cba9363a339917b3f352e4b3b4b1
kambara <1911140015d@ed.fuk.kindai.ac.jp>
2021-11-15 08:04:29 +0000

    current_fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     private
         def current_user
             if session[:login_student_num]
-                User.find_by(uid: session[:login_student_num])
+                User.find_by(student_num: session[:login_student_num])
             end
         end
     helper_method :current_user
