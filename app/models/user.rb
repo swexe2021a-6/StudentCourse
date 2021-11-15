1a1b73c5f67a0858baa28ddadfe6f7ddbdcc3c6b
kambara <1911140015d@ed.fuk.kindai.ac.jp>
2021-11-15 07:02:31 +0000

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     #TODO: validates
-    has_many :valuation, dependent: :destroy
+    #has_many :valuation, dependent: :destroy
     validates :password, presence: true, confirmation: true
     validates :student_num, uniqueness: true, presence: true
     attr_accessor :password, :password_confirmation
