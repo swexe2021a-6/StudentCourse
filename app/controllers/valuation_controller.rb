d5eaea6081603b8d3e3e609b06ec29cb74ab89b9
riki-ogita <7183riki@gmail.com>
2021-11-22 08:46:37 +0000

    index_view_edit

--- app/controllers/valuation_controller.rb
+++ app/controllers/valuation_controller.rb
@@ -9,7 +9,7 @@ class ValuationController < ApplicationController
     
     def create
         user = User.find_by(student_num: current_user.student_num)
-        @valuation = Valuation.new(comment: params[:valuation][:comment], user_id: user.id)
+        @valuation = Valuation.new(comment: params[:valuation][:comment],star: params[:valuation][:star],user_id: user.id)
         if @valuation.save
           #TODO: ツイートが成功したことをユーザに知らせる
           flash[:notice] = '評価入力'
