b4673fad461446f4c65b2656e1f1ee782084a49d
riki-ogita <7183riki@gmail.com>
2021-11-22 07:51:32 +0000

    valuation_index_view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :user
   #resources :valuation
   get 'valuation/index'
-  get 'valuation/indexS'
+  get 'valuation/indexView'
   get 'top/main'
   post 'top/login'
   get 'top/logout'
