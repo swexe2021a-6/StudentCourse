a9b47cd968ad9fe7aa77fc23793d55e5764a30f4
Ubuntu <1911140015d@ed.fuk.kindai.ac.jp>
2021-12-15 05:33:55 +0000

    平均値

--- app/controllers/valuationviews_controller.rb
+++ app/controllers/valuationviews_controller.rb
@@ -1,8 +1,6 @@
 class ValuationviewsController < ApplicationController
     def show
         @valuation = Valuation.where(subject: params[:id])
-        logger.debug("----------------------------------------------")
-        logger.debug(@valuation)
         #item.where(cart_id: current_cart.id)
     end
 end
