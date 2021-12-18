00da6341ab737e9026df2b99f17dc3d77b142865
RikiOgita <1911140059k@ed.fuk.kindai.ac.jp>
2021-12-18 09:36:40 +0000

    show_css

--- app/controllers/valuationviews_controller.rb
+++ app/controllers/valuationviews_controller.rb
@@ -1,5 +1,7 @@
 class ValuationviewsController < ApplicationController
     def show
+        @valuation = Valuation.all
+        @management = Management.all
         @valuation = Valuation.where(subject: params[:id])
         #item.where(cart_id: current_cart.id)
     end
