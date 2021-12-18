57d6cfb6b35677e2167ba7a4402fe8e4e5e7d3bf
Ubuntu <1911140015d@ed.fuk.kindai.ac.jp>
2021-12-18 08:54:04 +0000

    正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :valuation #, dependent: :destroy
     
     validates :password, presence: true, confirmation: true, length: { maximum: 8 }
-    validates :student_num, uniqueness: true, presence: true, length: { is: 10 }
+    validates :student_num, uniqueness: true, presence: true, length: { is: 10 },  format: { with: /[0-9]{2}+1114+[0-9]{4}/ }
     attr_accessor :password, :password_confirmation
 
   def password=(val)
