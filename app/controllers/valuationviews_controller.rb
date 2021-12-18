f666e684bc4cc43bb22fcdb1f1fa65f6f5593dd5
Ubuntu <1911140015d@ed.fuk.kindai.ac.jp>
2021-12-18 10:23:48 +0000

    A

--- app/controllers/valuationviews_controller.rb
+++ app/controllers/valuationviews_controller.rb
@@ -3,6 +3,5 @@ class ValuationviewsController < ApplicationController
         @management = Management.all
         @management = Management.find(params[:id])
         @valuation = Valuation.where(subject: params[:id])
-        #item.where(cart_id: current_cart.id)
     end
 end
